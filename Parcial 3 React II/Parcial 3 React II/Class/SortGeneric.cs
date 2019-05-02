640c9ad03dc9f759059dba836b1e8a8fa3c09912
Gustavo Vasquez Acosta <Gustalex99@hotmail.com>
2019-05-02 13:19:30 -0600

    !!!

---
+++ Parcial 3 React II/Parcial 3 React II/Class/SortGeneric.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_3_React_II.Class
+{
+    class SortGeneric
+    {
+        public static void insertionSort<T>(T[] arr) where T : IComparable
+        {
+            for (int i = 1; i<arr.Length; i++)
+            {
+                int j;
+                for (i = 1; i < arr.Length; i++)
+                {
+                    T key = arr[i];
+                    j = i - 1;
+
+                    while (j >= 0 && (arr[j].CompareTo(key) > 0))
+                    {
+                        arr[j + 1] = arr[j];
+                        j = j - 1;
+                    }
+                    arr[j + 1] = key;
+                }
+                }
+        }
+        
+
+    }
+}
--- Parcial 3 React II/Parcial 3 React II/Program.cs
+++ Parcial 3 React II/Parcial 3 React II/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Parcial_3_React_II.Class;
 
 namespace Parcial_3_React_II
 {
@@ -10,7 +11,57 @@ namespace Parcial_3_React_II
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("yas");
+            pal[] arr = { new pal("larga"), new pal("trousers"), new pal("melon"), new pal("hey"), new pal("girl"), new pal("m"), new pal("word"), new pal("word") };
+
+
+
+            SortGeneric.insertionSort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + "\n ");
         }
+
+
+            public class pal : IComparable
+            {
+            string pala;
+            public pal(string pala)
+            {
+                this.pala = pala;
+            }
+
+            public int CompareTo(object obj)
+            {
+                if ((((pal)obj).pala.Length) - (pala.Length) < 0)
+                {
+                    return 1;
+                }
+                if ((((pal)obj).pala.Length) - (pala.Length) == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            public override string ToString()
+            {
+                return pala;
+            }
+            }
+
+
+    
+            
     }
 }
+
+
+
+
+
+             
+      
+    
+
