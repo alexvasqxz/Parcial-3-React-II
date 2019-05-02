ad2e5a62ae359c7015597e9f5dd5d96c7347a34b
Gustavo Vasquez Acosta <Gustalex99@hotmail.com>
2019-05-02 12:52:58 -0600

    !

---
+++ Parcial 3 React II/Parcial 3 React II/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_3_React_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yas");
+        }
+    }
+}
